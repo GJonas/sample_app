93891d4aa3e90e90ac92abe906a809b006d2f063
Jonas <jonas2moreira@hotmail.com>
2013-10-24 18:51:06 -0200

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+	before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+	
 	before_action :correct_user,   only: [:edit, :update]
 	before_action :admin_user,     only: :destroy
-
-
-
 
 
 	def new
 		@user = User.new
 	end
+	
 	def show
 		@user = User.find(params[:id])
-		 @microposts = @user.microposts.paginate(page: params[:page])
+		@microposts = @user.microposts.paginate(page: params[:page])
 	end
 
 	def index
@@ -50,7 +49,19 @@ class UsersController < ApplicationController
 		flash[:success] = "User deleted."
 		redirect_to users_url
 	end
-	
+	def following
+   	@title = "Following"
+   	@user = User.find(params[:id])
+   	@users = @user.followed_users.paginate(page: params[:page])
+   	render 'show_follow'
+   end
+
+   def followers
+   	@title = "Followers"
+   	@user = User.find(params[:id])
+   	@users = @user.followers.paginate(page: params[:page])
+   	render 'show_follow'
+   end
 
 	private
 	def user_params
@@ -72,11 +83,10 @@ class UsersController < ApplicationController
    	redirect_to(root_url) unless current_user?(@user)
    end
 
+   
+
    private 
    def admin_user
    	redirect_to(root_url) unless current_user.admin?
    end
-
-
-
 end
